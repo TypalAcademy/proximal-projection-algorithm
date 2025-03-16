310ce871aef14fd578ba7f1a9e5d38500c0ed9f2
Howard Heaton <54549555+howardheaton@users.noreply.github.com>
2025-03-16 15:27:56 -0700

    Update stats.py

--- src/basis_pursuit/stats.py
+++ src/basis_pursuit/stats.py
@@ -10,26 +10,26 @@ class Stats(BaseModel):
     """Object for tracking stats of optimization execution"""
 
     matrix: (np.ndarray)
-    measurements: (np.array)
+    measurements: (np.ndarray)
     viol: List[float] = Field(default_factory=list)
     obj: List[float] = Field(default_factory=list)
     res: List[float] = Field(default_factory=list)
 
     def add_stat(self, x, x_p):
         """Update arrays storing performance statistics."""
-        viol = np.linalg.norm(self.matrix @ x - self.measurements)
+        viol = float(np.linalg.norm(self.matrix @ x - self.measurements))
         if not self.viol:
             self.viol = [viol]
         else:
             self.viol.append(viol)
 
-        obj = np.linalg.norm(x, ord=1)
+        obj = float(np.linalg.norm(x, ord=1))
         if not self.obj:
             self.obj = [obj]
         else:
             self.obj.append(obj)
 
-        res = np.linalg.norm(x - x_p)
+        res = float(np.linalg.norm(x - x_p))
         if not self.res:
             self.res = [res]
         else:
